04bfa1415c8dc6413e449138e8958533434f2f25
bia9400 <ferro.biagio@gmail.com>
2022-06-09 14:32:34 +0200

    fine

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 let x= new Vue ({
     el:"#app",
+    data:{
+        message:"Ciao",
+        imagLink:"https://www.stadionews.it/wp-content/uploads/2020/12/131972933_414929609863479_4849688280021351047_o.jpg"
+    }
 })
